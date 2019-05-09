aa98d43bd42583b93ff947f2f885a7b13cbaa07c
Rodrigo Miklos Vieira <rodrigo.vieira67@etec.sp.gov.br>
2019-05-08 22:16:46 -0300

    Criado app para criação de menu

---
+++ app/src/main/java/br/com/etecia/menuappetecia/Configuracoes_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menuappetecia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Configuracoes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuracoes_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/menuappetecia/Favoritos_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.menuappetecia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Favoritos_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favoritos_layout);
+    }
+}
